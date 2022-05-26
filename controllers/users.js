977030134473f5c89aea28d9a0763edcbfc33a4b
Nick Molina <Nicholasmolina15@gmail.com>
2022-05-26 15:25:46 -0500

    create reg route

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 const express = require('express');
+const res = require('express/lib/response');
 const userRouter = express.Router();
 const User = require('../models/user.js');
 
@@ -8,7 +9,11 @@ const User = require('../models/user.js');
 
 //create (registration router)
 
-
+    userRouter.post('/', (req, res) => {
+    //overwrite the user password with hashed password, then pass to database
+    req.body.passowrd = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+    res.send(req.body);
+}) 
 
 
 //export user router
